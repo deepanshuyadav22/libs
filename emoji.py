8ed67cb7fc8529b34fe4c9e02d8cc1458840340c
deepanshuyadav22 <59260686+deepanshuyadav22@users.noreply.github.com>
2021-04-18 21:17:30 +0530

    Create emoji.py

---
+++ emoji.py
@@ -0,0 +1,9 @@
+import emoji
+
+print(emoji.emojize(":thumbs_up:"))
+print(emoji.emojize(":red_heart:"))
+print(emoji.emojize(":star:"))
+print(emoji.emojize(":fire:"))
+print(emoji.emojize(":family:"))
+
+#https://www.webfx.com/tools/emoji-cheat-sheet/
