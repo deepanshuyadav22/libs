9d1646b9d641eda9243e3d134cd81442cbc71aa1
deepanshuyadav22 <59260686+deepanshuyadav22@users.noreply.github.com>
2021-04-19 22:56:20 +0530

    Create pywhatkit.py

---
+++ pywhatkit.py
@@ -0,0 +1,21 @@
+import pywhatkit as kit
+
+#send a WhatsApp msg
+#kit.sendwhatmsg("+91xxxxxxxxxx", "Hello.", 21, 55)
+
+#search on YouTube for the entered text
+#kit.playonyt("Python")
+
+#search on Google for the entered text
+#kit.search("Python")
+
+#a short info about the entered text (gives error on some words like Python, science, etc.)
+#kit.info("python")
+
+#path of input image, path of output text-file (if xyz.txt does not exist, it will create)
+#kit.image_to_ascii_art(r"E:/xyz.png", r"E:/xyz.txt")
+
+#texts, path and name of output image (supports png & jpg)
+#kit.text_to_handwriting("Hello", "E:/xyz.png")
+
+#https://pywhatkit.herokuapp.com/
