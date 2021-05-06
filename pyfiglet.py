561f2a29759cfa68f6544b5df27fe940e14ed3fc
deepanshuyadav22 <59260686+deepanshuyadav22@users.noreply.github.com>
2021-05-06 15:18:04 +0530

    Create pyfiglet.py

---
+++ pyfiglet.py
@@ -0,0 +1,22 @@
+import pyfiglet
+
+output = pyfiglet.figlet_format("Deepanshu")
+print(output)
+
+'''
+List of Fonts:
+1. slant
+2. 3-d
+3. 3x5
+4. 5lineoblique
+5. alphabet
+6. banner3-d
+7. doh
+8. isometric1
+9. letters
+10. alligator
+11. dotmatrix
+12. bubble
+13. bulbhead
+14. digital
+'''
